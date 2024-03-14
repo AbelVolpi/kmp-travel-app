6bc6980eb27d8dae5e0df47e98997362d4145e2c
abelvolpi <abelvolpi@gmail.com>
2024-03-14 17:33:25 -0300

    feat(network): add simple connection structure

--- androidApp/src/main/java/com/abelvolpi/kmptravelapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/abelvolpi/kmptravelapp/android/MainActivity.kt
@@ -3,38 +3,55 @@ package com.abelvolpi.kmptravelapp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.abelvolpi.kmptravelapp.Greeting
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
+            App()
         }
     }
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun App(mainViewModel: MainViewModel = viewModel()) {
+    MyApplicationTheme {
+        val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
+        GreetingView(greetings = greetings)
+    }
+}
+
+@Composable
+fun GreetingView(greetings: List<String>) {
+    Column(
+        modifier = Modifier.padding(all = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        greetings.forEach { greeting ->
+            Text(greeting)
+            Divider()
+        }
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView(
+            listOf("a", "b")
+        )
     }
 }
