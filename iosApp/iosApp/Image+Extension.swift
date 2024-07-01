c6d68fa9054ff91767c90dab4eeaa58d4929c183
Andrey de Lara <andrey.lara@premiersoft.net>
2024-07-01 20:34:58 -0300

    feat(ui): Add resource extensions

---
+++ iosApp/iosApp/Image+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Image+Extension.swift
+//  iosApp
+//
+//  Created by Andrey de Lara on 25/03/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+extension Image {
+    static var logoIcon: Image {
+        Image("Logo")
+    }
+}
