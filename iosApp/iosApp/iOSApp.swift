2cc1906be330710360c4322d8e86595b880d929f
Andrey de Lara <andrey.lara@premiersoft.net>
2024-07-01 20:35:57 -0300

    feat(ui): Add hardcoded screens

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,9 +2,28 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                NavigationStack {
+                    ContentView()
+                }
+                .tabItem {
+                    Label("", systemImage: "safari")
+                }
+                NavigationStack {
+                    InformationsView()
+                }
+                .tabItem {
+                    Label("", systemImage: "house")
+                }
+            }
+            .preferredColorScheme(.dark)
+            .accentColor(.white)
+            .onAppear {
+                UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+                UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            }
+        }
 	}
 }
