0ea0bf6484cbb7e354c4a84ccaa855e67e1bf718
abelvolpi <abelvolpi@gmail.com>
2024-10-16 09:35:15 +0100

    refactor: empty shared module tests

--- shared/src/androidUnitTest/kotlin/com/luacheia/kmptravelapp/CommonGreetingTest.kt
+++ shared/src/androidUnitTest/kotlin/com/luacheia/kmptravelapp/CommonGreetingTest.kt
@@ -1,11 +1,9 @@
 package com.luacheia.kmptravelapp
 
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class CommonGreetingTest {
     @Test
     fun testExample() {
-        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
     }
 }
