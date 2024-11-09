7a7b522bcd30c4d459741ae02143b6e9e8aca725
Andrey de Lara <andrey.lara@premiersoft.net>
2024-11-09 16:52:07 -0300

    refactor(ios): implements good practices in About Us section

---
+++ iosApp/iosApp/Screens/AboutUs/AboutUsState.swift
@@ -0,0 +1,13 @@
+//
+//  AboutUsState.swift
+//  iosApp
+//
+//  Created by Andrey de Lara on 09/11/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import Foundation
+
+struct AboutUsState {
+    var paragraphs: [String] = []
+}
