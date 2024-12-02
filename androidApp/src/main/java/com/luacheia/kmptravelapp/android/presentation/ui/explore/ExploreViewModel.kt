66f30711164b666b035ed87a947f5075800860cb
Andrey de Lara <andrey.lara@premiersoft.net>
2024-12-02 12:53:45 -0300

    feat(android-ui): implement search bar in ExploreScreen

--- androidApp/src/main/java/com/luacheia/kmptravelapp/android/presentation/ui/explore/ExploreViewModel.kt
+++ androidApp/src/main/java/com/luacheia/kmptravelapp/android/presentation/ui/explore/ExploreViewModel.kt
@@ -9,7 +9,11 @@ import com.luacheia.kmptravelapp.data.repository.PlaceRepository
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.time.debounce
 
 class ExploreViewModel(
     private val categoryRepository: CategoryRepository,
@@ -19,11 +23,15 @@ class ExploreViewModel(
     private val _exploreModel = MutableStateFlow<ExploreModel?>(null)
     val exploreModel: StateFlow<ExploreModel?> get() = _exploreModel
 
+    private val _searchText = MutableStateFlow("")
+    val searchText: StateFlow<String> = _searchText
+
     init {
         getExploreScreenData()
+        setupDebounceSearch()
     }
 
-    fun getExploreScreenData() {
+    private fun getExploreScreenData() {
         viewModelScope.launch {
             val placesDeferred = async {
                 var places = listOf<Place>()
@@ -40,6 +48,31 @@ class ExploreViewModel(
             val categories = categoriesDeferred.await()
 
             _exploreModel.value = ExploreModel(categories, places)
+        }
+    }
+
+    fun searchPlaces(searchText: String? = null) {
+        viewModelScope.launch {
+            placeRepository.getAllPlaces(searchText).collect { updatedPlaces ->
+                _exploreModel.value = _exploreModel.value?.copy(places = updatedPlaces)
+            }
+        }
+    }
+
+    fun onSearchQueryChange(searchText: String) {
+        _searchText.value = searchText
+    }
+
+    private fun setupDebounceSearch() {
+        viewModelScope.launch {
+            _searchText
+                .debounce(400)
+                .filter { it.trim().isNotEmpty() }
+                .distinctUntilChanged()
+                .collect {
+                    print("Passou")
+                    searchPlaces(it)
+                }
         }
     }
 }
